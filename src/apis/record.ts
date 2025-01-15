9d0bb6d061fd0aaacc8c7a4466b69df1b5f6b95a
Yousef Qawasmeh <qwasme.yousef@gmail.com>
2025-01-15 15:32:56 +0200

    prepare the old version to build

--- src/apis/record.ts
+++ src/apis/record.ts
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-const API_URL = 'https://super-market-eqe5.onrender.com'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 type Ifilters = {
     date?: string
     type?: number
@@ -10,27 +10,27 @@ type Ifilters = {
 }
 
 export const createNewRecord = async (newRecord: any) => {
-    const res = await Axios.post(`${API_URL}/api/records`, newRecord)
+    const res = await Axios.post(`${API_URL}/records`, newRecord)
     return res
 }
 
 export const getRecords = async (filters: Ifilters) => {
-    const res = await Axios.get(`${API_URL}/api/records`, {params: filters})
+    const res = await Axios.get(`${API_URL}/records`, {params: filters})
     return res
 }
 
 export const getRecordById = async (id: string) => {
-    const res = await Axios.get(`${API_URL}/api/records/${id}`)
+    const res = await Axios.get(`${API_URL}/records/${id}`)
     return res
 }
 
 export const deleteRecordById = async ({id, notes}:{id: string, notes: string}) => {
-    const res = await Axios.delete(`${API_URL}/api/records/${id}`, {data: {notes}})
+    const res = await Axios.delete(`${API_URL}/records/${id}`, {data: {notes}})
     return res
 }
 
 export const updateRecordById = async (id: string, newRecord: any) => {
-    const res = await Axios.put(`${API_URL}/api/records/${id}`, newRecord)
+    const res = await Axios.put(`${API_URL}/records/${id}`, newRecord)
     return res
 }
 
--- src/apis/recordsTypes.ts
+++ src/apis/recordsTypes.ts
@@ -1,27 +1,27 @@
 import Axios from 'axios'
-const API_URL = 'https://super-market-eqe5.onrender.com'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api'
 
 export const createNewRecordType = async (newRecordType: any) => {
-    const res = await Axios.post(`${API_URL}/api/recordtypes`, newRecordType)
+    const res = await Axios.post(`${API_URL}/recordtypes`, newRecordType)
     return res
 }
 
 export const getRecordTypes = async () => {
-    const res = await Axios.get(`${API_URL}/api/recordtypes`)
+    const res = await Axios.get(`${API_URL}/recordtypes`)
     return res
 }
 
 export const getRecordTypeById = async (id: string) => {
-    const res = await Axios.get(`${API_URL}/api/recordtypes/${id}`)
+    const res = await Axios.get(`${API_URL}/recordtypes/${id}`)
     return res
 }
 
 export const deleteRecordTypeById = async (id: string) => {
-    const res = await Axios.delete(`${API_URL}/api/recordtypes/${id}`)
+    const res = await Axios.delete(`${API_URL}/recordtypes/${id}`)
     return res
 }
 
 export const updateRecordTypeById = async (id: string, newRecordType: any) => {
-    const res = await Axios.put(`${API_URL}/api/recordtypes/${id}`, newRecordType)
+    const res = await Axios.put(`${API_URL}/recordtypes/${id}`, newRecordType)
     return res
 }
